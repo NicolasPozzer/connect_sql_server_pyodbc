1f500af678e72622f60e558e4c9f988e99c979f1
NicolasPozzer <pozzernico@gmail.com>
2024-06-13 15:18:50 -0300

    mask support .csv and .txt

--- src/data-masking/data-masking_csv.py
+++ src/data-masking/data-masking_csv.py
@@ -1,11 +1,7 @@
 import pandas as pd
 from faker import Faker
 
-try:
-    df = pd.read_csv("data-in.csv")
-    print(df.head())
-except Exception as ex:
-    print("Error when trying to read data",ex)
+name_file = "data-in.txt" # Reads data from a .csv or .txt file.
 
 # Create instance Faker
 fake = Faker()
@@ -23,13 +19,28 @@ def mask_phone_number():
     return fake.phone_number()
 
 # Mask Data
-df['ID'] = df['ID'].apply(lambda x: mask_id())
-df['Name'] = df['Name'].apply(lambda x: mask_name())
-df['Last Name'] = df['Last Name'].apply(lambda x: mask_last_name())
-df['Phone'] = df['Phone'].apply(lambda x: mask_phone_number())
+def mask(file):
+    file['ID'] = file['ID'].apply(lambda x: mask_id())
+    file['Name'] = file['Name'].apply(lambda x: mask_name())
+    file['Last Name'] = file['Last Name'].apply(lambda x: mask_last_name())
+    file['Phone'] = file['Phone'].apply(lambda x: mask_phone_number())
 
-print(df.head())
+def outputData(filepath):
+    print("---------------output data-----------------", "\n", file.head())
 
-#save data
-df.to_csv('data-out.csv', index=False)
+    if filepath.endswith(".csv"):
+        file.to_csv('data-out.csv', index=False)
+    else:
+        file.to_csv('data-out.txt', index=False)
 
+
+# RUN #
+try:
+    file = pd.read_csv(name_file)
+    print("\n","---------------input data-----------------")
+    print(file.head(),"\n")
+    mask(file)
+    outputData(name_file) # save data
+
+except Exception as ex:
+    print("Error when trying to read data",ex)
