c9df713bdd29541f8022376144683a4ba52a01a4
NicolasPozzer <pozzernico@gmail.com>
2024-06-14 11:21:07 -0300

    fix sql server queries

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
-import pyodbc as db # Lo nombro como DB
+import pyodbc as db
 
-# Defino los parámetros de conexión y
-# Conecto a SQL Server usando la autenticación de Windows
+# Conectar a SQL Server usando la autenticación de Windows
 try:
     conexion = db.connect(
         driver="{SQL Server}",
@@ -12,33 +11,38 @@ try:
 except db.Error as ex:
     print(f"Error al conectar: {ex}")
 
-# CONSULTAS..
-queries = \
-    [
-        "INSERT INTO MiTabla (ID, Mensaje) VALUES (5, '5to Elemento');",
-        "SELECT * FROM MiTabla;"
-    ]
+# Consulta de inserción
+insert_query = """
+INSERT INTO OtraTabla (ID, Mensaje)
+SELECT ID, Mensaje
+FROM MiTabla;
+"""
 
 # Ejecuto la consulta de inserción y confirmo la transacción
 try:
-    cursor = conexion.cursor() #Utilizo cursor para manejar la conexion
-    for item in queries:
-        cursor.execute(item)
+    cursor = conexion.cursor()  # Utilizo cursor para manejar la conexión
 
-    #conexion.commit()  #Con commit confirmo la transacción si no lo implemento puedo ver como va a quedar
+    # Ejecutar la consulta de inserción
+    cursor.execute(insert_query)
+
+    # Confirmar la transacción
+    conexion.commit()  # Con commit confirmo la transacción
     print("Inserción realizada con éxito")
 
-    # guardar en una variable -> result
+    # Consulta de selección para verificar los datos insertados
+    select_query = "SELECT ID, Mensaje FROM OtraTabla;"
+
+    cursor.execute(select_query)
+
+    # Obtener los resultados
     result = cursor.fetchall()
 
-    #print(result) -> Imprimir de forma simple pero en una sola linea.
-    # Recorrer e imprimir de manera clara y ordenada.
+    # Imprimir los resultados de manera clara y ordenada
     for row in result:
         print(row)
 except db.Error as ex:
     print(f"Error al ejecutar la consulta: {ex}")
-
 finally:
-    # Cerrar la conexión Aunque ocurran errores o no.
+    # Cerrar la conexión aunque ocurran errores o no.
     if 'conexion' in locals():
         conexion.close()
